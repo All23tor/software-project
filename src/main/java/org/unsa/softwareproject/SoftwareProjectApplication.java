f7d3f7d4c4d240ad01f0f2a9a19c5da82b442392
Arthur-Meza1 <amezapa@unsa.edu.pe>
2025-07-25 12:48:53 -0500

    Se arregló el main function

---
+++ src/main/java/org/unsa/softwareproject/SoftwareProjectApplication.java
@@ -0,0 +1,15 @@
+package org.unsa.softwareproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+@SpringBootApplication
+public class SoftwareProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareProjectApplication.class, args);
+	}
+}
